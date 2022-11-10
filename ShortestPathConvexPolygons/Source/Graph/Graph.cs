7ab3c04476a68ddf0dd9f7633785e0ea33009a04
afonamos <d.splinter@live.com>
2022-11-10 13:40:09 -0500

    grammar

--- ShortestPathConvexPolygons/Source/Graph/Graph.cs
+++ ShortestPathConvexPolygons/Source/Graph/Graph.cs
@@ -22,8 +22,8 @@ public partial class Graph {
     public struct Node {
         public Vec2 vertex;
         public List<Vec2> edges;
-        public List<Node> neighbors; //Slightly scuffed holds both edges and visible nodes, makes graph traversal code easier at the cost of memory waste
-        public Polygon poly; //The polygon an object is a part of to prevent internal angles (there is a better way to do this involving checking edge angles)
+        public List<Node> neighbors; //Contains all nodes that are visible to the current node (includes edges)
+        public Polygon poly; //To prevent internal neighbors (there is a better way to do this involving checking edge angles(too lazy at the moment))
 
         public Node(Vec2 vertex, List<Vec2> edges, List<Node> neighbors, Polygon poly) {
             this.vertex = vertex;
